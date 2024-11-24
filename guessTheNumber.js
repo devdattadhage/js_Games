f665b2585e96a5ba338ad437aebe009dc06c3805
Devdatta Dhage <devdattadhage1909@gmail.com>
2024-11-24 16:52:22 +0530

    Update guessTheNumber.js

--- guessTheNumber.js
+++ guessTheNumber.js
@@ -3,35 +3,33 @@ const rangeEnd = 10;
 const maxAttempts = 3;
 
 function generateNumber() {
-  return Math.floor(Math.random() * 10);
+  return Math.ceil(Math.random() * 10);
 }
 
-function welcomeMessage() {
+function initiateGuessTheNumber(maxAttempts, number) {
+  if (maxAttempts < 1) {
+    return "\n  Better Luck Next Time!!";
+  }
+
+  const userInput = +prompt("\n\n  Enter a Number : ");
+  if (userInput === number) {
+    return "\n Hurray!! You Guessed the Number!";
+  }
+
+  const hint = userInput > number ? "  Too High " : "  Too Low ";
+  console.log(hint);
+
+  return initiateGuessTheNumber(maxAttempts - 1, number);
+}
+
+function gameInfo() {
   const welMsg = "\n  Welcome to Guess the Number! ";
-  const rangeInfo = "\n  The secret number is between 0 and 10.";
+  const rangeInfo = "\n  The secret number is between 1 and 10.";
   const attemptsInfo = "\n  You have 3 attempts to find it.";
 
   return welMsg + rangeInfo + attemptsInfo;
 }
 
-function gameStart(maxAttempts, number) {
-  const userInput = +prompt("\n\n  Enter a Number : ");
-
-  if (userInput === number) {
-    return "\n Hurray!! You Guessed the Number!";
-  }
-
-  const msg = userInput > number ? "  Too High " : "  Too Low ";
-  console.log(msg);
-
-  if (maxAttempts < 2) {
-    return "\n  Better Luck Next Time!!";
-  }
-  
-  return gameStart(maxAttempts - 1, number);
-}
-
-console.log(welcomeMessage());
 const number = generateNumber();
-
-console.log(gameStart(maxAttempts, number));
+console.log(gameInfo());
+console.log(initiateGuessTheNumber(maxAttempts, number));
